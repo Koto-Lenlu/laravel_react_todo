a5ccfa41de2d270cad07633e37eb1272dfd264da
Koto-Lenlu <takeda.koto.555@gmail.com>
2021-06-19 16:59:31 +0900

    Material-UIの導入

--- backend/resources/js/components/Example.js
+++ backend/resources/js/components/Example.js
@@ -1,5 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core'; //Buttonをインポート
 
 function Example() {
     return (
@@ -7,9 +9,11 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">React導入できたわな？？</div>
+                        <div className="card-header">React導入できたわな</div>
 
-                        <div className="card-body">I'm an  component!</div>
+                        <div className="card-body">Im an example component!</div>
+                        //ボタンを追記
+                        <Button color="secondary" variant="contained">ワイがMaterial-UIのボタンやな？？</Button>
                     </div>
                 </div>
             </div>
@@ -22,3 +26,4 @@ export default Example;
 if (document.getElementById('app')) {
     ReactDOM.render(<Example />, document.getElementById('app'));
 }
+
